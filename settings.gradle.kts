dd18c5b6d6bcb7df1d9ddc841a9cb634adfe53a4
raviipentaa1947-netizen <raviipentaa1947@gmail.com>
2025-08-16 21:15:14 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
-// placeholder for settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Ravi"
+include(":app")
